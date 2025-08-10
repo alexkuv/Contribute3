91e90312315942f9d035ed5912d53e7f3ebbcde3
Alex Kuvaldin <a.kuvaldin@make.st>
2025-08-11 03:33:42 +0700

    add health endpont

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ContributionModule } from './contributions/contribution.module';
+import * as dotenv from 'dotenv';
+import { AppController } from '../src/app.controller';
 import { AppService } from './app.service';
 
+dotenv.config();
+
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017/contribute3'),
+    ContributionModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
